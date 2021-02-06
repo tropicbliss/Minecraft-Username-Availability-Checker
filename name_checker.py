cca536b65f1668c3f3a5cc9349bd6f504535494c
chronicallyunfunny <48951297+chronicallyunfunny@users.noreply.github.com>
2021-02-06 23:07:21 +0800

    Update name_checker.py

--- name_checker.py
+++ name_checker.py
@@ -36,7 +36,7 @@ def main():
                 print("{} is available at {} ({} later).".format(username, available_time, wait_time))
                 available_names.append(username)
             except AttributeError:
-                print("{} is unavailable or has never been used before.".format(username))
+                print("{} is unavailable or have never been used before.".format(username))
             finally:
                 if retry == False:
                     if ((is_valid_char(username) == False) or (len(username) < 3) or (len(username) > 16)):
